7fbd517dcc514e4f7c53204784f1c5bc678b8a7e
Eric McDaniel <eric.g.mcdaniel@gmail.com>
2022-05-17 15:32:30 -0400

    fix: transform empty table_rows from prosemirror

--- src/transform/transformers/table/fromProsemirror.ts
+++ src/transform/transformers/table/fromProsemirror.ts
@@ -81,7 +81,10 @@ const transformRow = (
     return {
         type: "Row",
         attr: createAttr(),
-        cells: cells.map((cell) => transformCell(cell, context)),
+        // Table rows may be devoid of content, for example phantom rows that
+        // are automatically added to satisfy a row where each element has
+        // rowspan >1
+        cells: cells?.map((cell) => transformCell(cell, context)) ?? [] ,
     };
 };
 
